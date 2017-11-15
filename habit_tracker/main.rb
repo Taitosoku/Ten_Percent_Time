b44d97e4ad2795935343bb84c329737c05548223
Lee <sariasgrace@gmail.com>
2017-11-14 22:00:52 -0500

    [new project] skeleton of my personalized habit tracker 	      including rspec tests 	      including creation and connection to psql db

---
+++ habit_tracker/main.rb
@@ -0,0 +1,10 @@
+# this class will handle the managng of habits
+
+def add_habit(habit_name)
+end
+
+def remove_habit(habit_id)
+end
+
+def update_habit(habit_id)
+end
---
+++ habit_tracker/spec/connector_spec.rb
@@ -0,0 +1,26 @@
+# tests the connector class
+
+require 'rspec'
+require 'connector'
+
+describe 'Connector' do
+  it 'sucessfully connects to the psql db' do
+
+  end
+
+  it 'can add a task to the habits table' do
+
+  end
+
+  it 'can remove a task from the habits table' do
+
+  end
+
+  it 'can query a list of habits linked to a specific user' do
+
+  end
+
+  it 'can edit a habit' do
+
+  end
+end
